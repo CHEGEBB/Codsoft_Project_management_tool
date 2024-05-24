82e11123b8f92bf0efe5b3eb46411a909b18e96d
CHEGEBB <chegephil24@gmail.com>
2024-05-24 20:37:00 +0300

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://codsoft-project-management-tool-1.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [react()]
 });
